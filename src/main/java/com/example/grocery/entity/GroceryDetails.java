33a5bb25f9a2ea63fe597f09d5ed8294f2561bd7
kushani <kushani.hansika@axiatadigitallabs.com>
2024-01-15 09:38:43 +0530

    updated entity

--- src/main/java/com/example/grocery/entity/GroceryDetails.java
+++ src/main/java/com/example/grocery/entity/GroceryDetails.java
@@ -1,11 +1,14 @@
 package com.example.grocery.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "planed_menus")
 @Data
 public class GroceryDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
